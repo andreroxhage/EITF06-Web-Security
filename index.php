9e64e94e5a96304db4f1ec34695c0446f281bfde
andreroxhage <an8603ro-s@student.lu.se>
2023-11-09 11:49:32 +0100

    started with session_start() implementation

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -21,8 +25,16 @@
             </ul>
         </nav>
         <main>
-            <h2>Welcome to Your Web Shop!</h2>
-            <!-- Add your main content here -->
+            <h2 id="signup-h2">Welcome to Your Web Shop!</h2>
+            <?php
+    // Check if the user is logged in
+    if (isset($_SESSION['user_id'])) {
+        echo 'Welcome, ' . $_SESSION['username'] . '!';
+        // Add other content for logged-in users
+    } else {
+        echo '<p id="signup-h2">Welcome, Guest!</p> <a href="login.php" id="signup-h2">Please login</a>.';
+    }
+    ?>
         </main>
         <footer>
             <p>&copy; 2023 Web Shop</p>
