d9fb7fddab001ca28e1e412efcc8dc97ff5d7bc8
andreroxhage <an8603ro-s@student.lu.se>
2023-11-09 10:25:02 +0100

    Initial web shop set-up

---
+++ authenticate.php
@@ -0,0 +1,6 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Handle user authentication logic, including password verification
+    // Redirect to appropriate page after authentication
+}
+?>
---
+++ register.php
@@ -0,0 +1,6 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Handle user registration logic, including password hashing and database insertion
+    // Redirect to appropriate page after registration
+}
+?>
