9a3ce61c56575c9267b2c29735a03f80896f17ae
andreroxhage <an8603ro-s@student.lu.se>
2023-11-16 13:32:34 +0100

    Measures to prevent CSRF

--- register.php
+++ register.php
@@ -1,8 +1,17 @@
 <?php
-// Handle user registration logic, including password hashing and database insertion
 // Set session timeout to 30 minutes
 ini_set('session.gc_maxlifetime', 1800);
+session_set_cookie_params([
+    'lifetime' => 1800, // match your session timeout
+    'path' => '/',
+    'domain' => '',
+    'secure' => true, // Send cookie only over HTTPS
+    'httponly' => true, // Prevent client-side access to the cookie
+    'samesite' => 'Strict', // prevents cookie from being sent by the browser with cross-site requests. Prevents CSRF-attacks
+]);
 session_start();
+
+// Handle user registration logic, including password hashing and database insertion
 
 // Function to check if the signup rate limit is exceeded
 function isSignupLimited() {
